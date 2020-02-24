7dc843cec67742c5106e7e9a5ee14b22727a2c9a
Joseph Omitogun <joseyi2013@gmail.com>
2020-02-24 08:37:04 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import express from "express";
 import morgan from "morgan";
 import configuration from "./config";
 
+
 const configure = (cb) => {
   cb(morgan, express);
 };
+const port =process.env.PORT||3000;
 
 const app = express();
 
 configure(configuration(app));
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log("Express server is running");
 });
 
