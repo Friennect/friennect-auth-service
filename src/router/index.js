63b0b1c5308299555e2a2d299ef5e34f0fb18af7
Kingsley Victor <javaprodigy56@gmail.com>
2020-02-25 21:18:50 +0100

    chore: add a few lines

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,10 @@ const router = Router();
 router.get("/", (req, res) => {
   res.status(200).json({
     statusCode: 200,
-    body: "Welcome to the Friennect Auth API"
+    body: "Welcome to the Friennect Auth API",
+    routes: {
+      prefix: "/api/v1"
+    }
   });
 });
 
