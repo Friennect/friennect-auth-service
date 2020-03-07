403e82cce454219ae8ce2fad18cc18be1cd40097
Kingsley Victor <javaprodigy56@gmail.com>
2020-03-07 16:14:10 +0100

    Write edge tests for sign in feature

--- src/test/auth.test.js
+++ src/test/auth.test.js
@@ -30,6 +30,47 @@ describe("TESTS", () => {
           done(err);
         });
     });
+    it("should log a user in", (done) => {
+      request(app)
+        .post(`${root}/login`)
+        .send({ email, password })
+        .end((err, res) => {
+          const { status, body } = res;
+          console.table([body.body]);
+          token = body.body.token;
+          console.log(token);
+          expect(status).to.be.eql(200);
+          done(err);
+        });
+    });
+    it("should respond with a 404 if user doesn't exist", (done) => {
+      request(app)
+        .post(`${root}/login`)
+        .send({
+          email: "wrongdetail@app.com",
+          password
+        })
+        .end((err, res) => {
+          const { status, body } = res;
+          console.table([body.body]);
+          expect(status).to.be.eql(404);
+          done(err);
+        });
+    });
+    it("should respond with a 400 if password is incorrect", (done) => {
+      request(app)
+        .post(`${root}/login`)
+        .send({
+          email,
+          password: "incorrectpassword"
+        })
+        .end((err, res) => {
+          const { status, body } = res;
+          console.table([body.body]);
+          expect(status).to.be.eql(400);
+          done(err);
+        });
+    });
   });
   after((done) => {
     Auth.destroy({ where: { email } }).then(() => {
